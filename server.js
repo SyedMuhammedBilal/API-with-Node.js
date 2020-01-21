51cd3ac3ff381d1a5e3810c1f518020c02c1306f
Syed Muhammed Bilal <intergo878@gmail.com>
2020-01-22 00:07:24 +0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require ('express');
+const MongoClient = require ('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+const db = require('./config/db');
+
+const app = express();
+
+const port = 8000;
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+MongoClient.connect('mongodb://localhost/notable', (err, database) => {
+    if (err) return console.log(err)
+    require('./app/routes')(app, database);
+    app.listen(port, () => {
+        console.log("we are live on " + port);
+    })
+})
